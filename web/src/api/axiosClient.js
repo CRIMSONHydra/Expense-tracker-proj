72d97a6c060aa254139bcb6dc35f44a13374b97a
CRIMSONHydra <naverdo1@gmail.com>
2025-10-25 14:05:41 +0530

    update backend url

--- web/src/api/axiosClient.js
+++ web/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3001/api', // Adjust if your backend port is different
+  baseURL: 'https://expense-tracker-proj.onrender.com:3001/api', // Adjust if your backend port is different
 });
 
 // Interceptor to add the token to every request
