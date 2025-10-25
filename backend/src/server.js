62784c39b607629d6cd68d41d5d20ab091bd4ce0
CRIMSONHydra <naverdo1@gmail.com>
2025-10-25 14:13:14 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
 import { initDB } from './config/db.js';
 import transactionRoutes from './routes/transactions.route.js';
 import rateLimiter from './middlewares/rateLimiter.js';
 import authRoutes from './routes/auth.route.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const __dirname = path.resolve();
 const app = express();
 
 //setup
@@ -25,11 +22,9 @@ const PORT = process.env.PORT || 3001;
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
 
-const frontendDistPath = path.join(__dirname, '../web/dist');
-app.use(express.static(frontendDistPath));
-
+app.use(express.static(path.join(__dirname, "../web/dist")));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(frontendDistPath, 'index.html'));
+  res.sendFile(path.join(__dirname, '../web/dist/index.html'));
 });
 
 //Connect db and start server
